2007e06122b8d89fa47548bb7d03d39287b89e5e
Smit Rao <smit.rao@mail.utoronto.ca>
2019-12-17 18:29:04 -0500

    Check for source-target connections before calculating initial flow

--- src/main/java/practice/local/graphstructures/FlowNetwork.java
+++ src/main/java/practice/local/graphstructures/FlowNetwork.java
@@ -3,12 +3,13 @@ package practice.local.graphstructures;
 public class FlowNetwork extends Graph {
     private Vertex source;
     private Vertex target;
-    private int currentFlow = 0;
+    private int currentFlow;
 
     public FlowNetwork(Vertex s, Vertex t) {
         super();
         this.source = s;
         this.target = t;
+        this.currentFlow = s.connectsTo(t) ? s.weightTo(t) : 0;
     }
 
     public Vertex getSource() {
